b884f5236733b4a325903562ee2211bf007c387b
Inferno tower from clash of clans <st23530@howick.school.nz>
2026-07-23 13:40:25 +1200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,21 @@
+markbook = {}
 
+def add_records(markbook):
+    x = True
+    y = True
+    while x:
+        name = input("Input the student's name: ")
+        if name:
+            x = False
+        else:
+            print("Name cannot be empty: ")
+
+    while y:
+        try: 
+            score = int(input("Input the student's score\n(Must be between 0 and 100 and cannot be empty): "))
+            if score:
+            
+            else:
+                print("Score needs to be a whole number!: ")
+        except ValueError:
+            print("Score needs to be a whole number: ")
