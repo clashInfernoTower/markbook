a9b3773251cf4c633ef62aea844ffb6fa9cc59b9
Inferno tower from clash of clans <st23530@howick.school.nz>
2026-07-24 12:40:12 +1200

    Gave variables more meaningful names for better clarity of their purpose.

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
 markbook = {}
 
 def add_records(markbook):
-    x = True
-    y = True
-    while x:
+    add_student = True
+    add_score = True
+    
+    while add_student == True:
         name = input("Input the student's name: ")
         if name:
-            x = False
+            add_student = False
         else:
             print("Name cannot be empty: ")
 
-    while y:
+    while add_score == True:
         try: 
             score = int(input("Input the student's score\n(Must be between 0 and 100 and cannot be empty): "))
             if score:
-            
+                match score:
+                    case int() if score > 0 and score < 100:
+                        markbook.update(Student=name, Score=score)
+                        add_score = False
+                        
             else:
                 print("Score needs to be a whole number!: ")
         except ValueError:
             print("Score needs to be a whole number: ")
+    print(markbook)
+
+
+add_records(markbook)
